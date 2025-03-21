1ab6aa0325dfa0257392b03ce3fff4f1ef141856
Administrator <Administrator@B508PC45>
2025-03-21 16:12:41 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class SimpleEvent : Form
+    {
+        public SimpleEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button was clicked");
+        }
+
+        private void SimpleEvent_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi, Welcome to C# 2010 programming");
+        }
+
+        private void SimpleEvent_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form was clicked");
+        }
+    }
+}
